9d54eee514542ad97ca4bfffae562008bbabcc9b
Nicolas Bouème <nicolasboueme@gmail.com>
2018-02-22 23:35:28 +0100

    Correction of getTotalCredit() method and set scale to BigDecimal variables to validate the isEquilibree() test

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -1,6 +1,7 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.junit.Assert;
@@ -10,8 +11,8 @@ import org.junit.Test;
 public class EcritureComptableTest {
 
     private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
-        BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
-        BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit);
+        BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit).setScale(2, RoundingMode.CEILING);
+        BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit).setScale(2, RoundingMode.CEILING);
         String vLibelle = ObjectUtils.defaultIfNull(vDebit, BigDecimal.ZERO)
                                      .subtract(ObjectUtils.defaultIfNull(vCredit, BigDecimal.ZERO)).toPlainString();
         LigneEcritureComptable vRetour = new LigneEcritureComptable(new CompteComptable(pCompteComptableNumero),
@@ -22,8 +23,7 @@ public class EcritureComptableTest {
 
     @Test
     public void isEquilibree() {
-        EcritureComptable vEcriture;
-        vEcriture = new EcritureComptable();
+        EcritureComptable vEcriture = new EcritureComptable();
 
         vEcriture.setLibelle("Equilibrée");
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
